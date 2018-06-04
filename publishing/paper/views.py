2d4eec0879c9ea867fafd4404c197f02e76a0b6a
Djordje Ilic <djordjeilic555@gmail.com>
2018-06-04 20:29:57 +0200

    Osnovna struktura funkcija

---
+++ publishing/paper/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def uploadPage(request):
+    pass
+
+def upload(request):
+    pass
+
+def revisionPage(request):
+    pass
+
+def uploadRevision(request):
+    pass
+
+def pullPaper(request):
+    pass
+
+def searchPage(request):
+    pass
+
+def myPapers(request):
+    pass
+
+def getPaper(request):
+    pass
+
+def pendingRevisions(request):
+    pass
+
+def acceptRevision(request):
+    pass
+
+def refuseRevision(request):
+    pass
+
+def appointRevisionPage(request):
+    pass
+
+def appointRevision(request):
+    pass
+
+def managePage(request):
+    pass
+
+def changeState(request):
+    pass
+
+def setAcceptedState(request):
+    pass
+
+def setRefusedState(request):
+    pass
+
+def setRevisionState(request):
+    pass
+
+def setWritingState(request):
+    pass
